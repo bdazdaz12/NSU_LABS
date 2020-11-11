c8884dca13ff8a698f7cbcb7bac619f6154071d2
yevsyukof <60897283+yevsyukof@users.noreply.github.com>
2020-11-11 23:34:10 +0700

    добавил новые тесты

--- second-year/c++/lab1/Tests.cpp
+++ second-year/c++/lab1/Tests.cpp
@@ -6,7 +6,7 @@ TEST(MemoryAllocationTest, ConstructorAllocationTest) {
     for (int i = 0; i <= 1000; ++i) {
         TritSet set(i);
         size_t allocLength = set.capacity();
-        EXPECT_GE(allocLength, i * 2 / 8 / sizeof(uint));
+        ASSERT_GE(allocLength, i * 2 / 8 / sizeof(uint));
     }
 }
 
@@ -36,21 +36,21 @@ TEST(MemoryAllocationTest, UnknownOutOfBound) {
     TritSet set(1000);
     size_t allocLength = set.capacity();
     set[1000000000] = Trit::Unknown;
-    EXPECT_EQ(allocLength, set.capacity());
+    ASSERT_EQ(allocLength, set.capacity());
 }
 
 TEST(MemoryAllocationTest, ComparingOutOfBound){
     TritSet set(1000);
     size_t allocLength = set.capacity();
-    EXPECT_FALSE(set[2000000000] == Trit::True);
-    EXPECT_EQ(allocLength, set.capacity());
+    ASSERT_FALSE(set[2000000000] == Trit::True);
+    ASSERT_EQ(allocLength, set.capacity());
 }
 
 TEST(MemoryAllocationTest, OutOfBoundAllocation) {
     TritSet set(1000);
     size_t allocLength = set.capacity();
     set[1000000000] = Trit::True;
-    EXPECT_LT(allocLength, set.capacity());
+    ASSERT_LT(allocLength, set.capacity());
 }
 
 TEST(MemoryAllocationTest, ShrinkMemoryFree) {
@@ -61,15 +61,15 @@ TEST(MemoryAllocationTest, ShrinkMemoryFree) {
     set[secondBlockEndIdx] = Trit::True;
     set.shrink();
     uLL expectedCapacity = ceil((2 * (secondBlockEndIdx + 1)) / (8.0 * sizeof(uint)));
-    EXPECT_EQ(expectedCapacity, set.capacity());
-    EXPECT_EQ(secondBlockEndIdx + 1, set.size());
+    ASSERT_EQ(expectedCapacity, set.capacity());
+    ASSERT_EQ(secondBlockEndIdx + 1, set.size());
 }
 
 TEST(MemoryAllocationTest, tritOperationAllocation) {
     TritSet a(1000);
     TritSet b(2000);
     TritSet c = a | b;
-    EXPECT_EQ(b.capacity(), c.capacity());
+    ASSERT_EQ(b.capacity(), c.capacity());
 }
 
 TEST(TritSetOperationsTest, OrTest) {
@@ -82,9 +82,9 @@ TEST(TritSetOperationsTest, OrTest) {
     b[1] = Trit::False;
     b[2] = Trit::Unknown;
     TritSet c = a | b;
-    EXPECT_EQ(c[0], Trit::True);
-    EXPECT_EQ(c[1], Trit::Unknown);
-    EXPECT_EQ(c[2], Trit::Unknown);
+    ASSERT_EQ(c[0], Trit::True);
+    ASSERT_EQ(c[1], Trit::Unknown);
+    ASSERT_EQ(c[2], Trit::Unknown);
 }
 
 TEST(TritSetOperationsTest, AndTest) {
@@ -97,9 +97,9 @@ TEST(TritSetOperationsTest, AndTest) {
     b[1] = Trit::False;
     b[2] = Trit::Unknown;
     TritSet c = a & b;
-    EXPECT_EQ(c[0], Trit::Unknown);
-    EXPECT_EQ(c[1], Trit::False);
-    EXPECT_EQ(c[2], Trit::Unknown);
+    ASSERT_EQ(c[0], Trit::Unknown);
+    ASSERT_EQ(c[1], Trit::False);
+    ASSERT_EQ(c[2], Trit::Unknown);
 }
 
 TEST(TritSetOperationsTest, NotTest) {
@@ -108,9 +108,52 @@ TEST(TritSetOperationsTest, NotTest) {
     a[1] = Trit::False;
     a[2] = Trit::Unknown;
     TritSet b = !a;
-    EXPECT_EQ(b[0], Trit::False);
-    EXPECT_EQ(b[1], Trit::True);
-    EXPECT_EQ(b[2], Trit::Unknown);
+    ASSERT_EQ(b[0], Trit::False);
+    ASSERT_EQ(b[1], Trit::True);
+    ASSERT_EQ(b[2], Trit::Unknown);
+}
+
+TEST(TritSetOperationsTest, assignmentTest) {
+    TritSet a(3);
+    a[0] = Trit::False;
+    a[1] = Trit::Unknown;
+    a[2] = Trit::True;
+    ASSERT_EQ(a[0], Trit::False);
+    ASSERT_EQ(a[1], Trit::Unknown);
+    ASSERT_EQ(a[2], Trit::True);
+}
+
+TEST(TritSetOperationsTest, assignmentOrTest) {
+    TritSet a(3);
+    a[0] |= Trit::False;
+    a[1] |= Trit::Unknown;
+    a[2] |= Trit::True;
+    ASSERT_EQ(a[0], Trit::Unknown);
+    ASSERT_EQ(a[1], Trit::Unknown);
+    ASSERT_EQ(a[2], Trit::True);
+}
+
+TEST(TritSetOperationsTest, assignmentAndTest) {
+    TritSet a(3);
+    a[0] &= Trit::False;
+    a[1] &= Trit::Unknown;
+    a[2] &= Trit::True;
+    ASSERT_EQ(a[0], Trit::False);
+    ASSERT_EQ(a[1], Trit::Unknown);
+    ASSERT_EQ(a[2], Trit::Unknown);
+}
+
+TEST(TritSetOperationsTest, tritInSetAccessTest) {
+    TritSet a(3);
+    a[0] = Trit::False;
+    a[1] = Trit::Unknown;
+    a[2] = Trit::True;
+    Trit e0 = a[0];
+    Trit e1 = a[1];
+    Trit e2 = a[2];
+    ASSERT_EQ(e0, Trit::False);
+    ASSERT_EQ(e1, Trit::Unknown);
+    ASSERT_EQ(e2, Trit::True);
 }
 
 TEST(TritSetFunctionsTests, CardinalityTest) {
@@ -118,9 +161,9 @@ TEST(TritSetFunctionsTests, CardinalityTest) {
     set[0] = Trit::False;
     set[1] = Trit::Unknown;
     set[2] = Trit::True;
-    EXPECT_EQ(set.cardinality(Trit::False), 1);
-    EXPECT_EQ(set.cardinality(Trit::Unknown), 1);
-    EXPECT_EQ(set.cardinality(Trit::True), 1);
+    ASSERT_EQ(set.cardinality(Trit::False), 1);
+    ASSERT_EQ(set.cardinality(Trit::Unknown), 1);
+    ASSERT_EQ(set.cardinality(Trit::True), 1);
 }
 
 TEST(TritSetFunctionsTests, cardinalityMapTest) {
@@ -137,9 +180,9 @@ TEST(TritSetFunctionsTests, cardinalityMapTest) {
     set[9] = Trit::Unknown;
     set[10] = Trit::Unknown;
     auto cardinalityMap = set.cardinality();
-    EXPECT_EQ(cardinalityMap[Trit::False], 3);
-    EXPECT_EQ(cardinalityMap[Trit::Unknown], 2);
-    EXPECT_EQ(cardinalityMap[Trit::True], 1);
+    ASSERT_EQ(cardinalityMap[Trit::False], 3);
+    ASSERT_EQ(cardinalityMap[Trit::Unknown], 2);
+    ASSERT_EQ(cardinalityMap[Trit::True], 1);
 }
 
 TEST(TritSetFunctionsTests, trimTest) {
@@ -152,13 +195,13 @@ TEST(TritSetFunctionsTests, trimTest) {
     set[5] = Trit::True;
     set[6] = Trit::Unknown;
     set.trim(2);
-    EXPECT_EQ(set[0], Trit::True);
-    EXPECT_EQ(set[1], Trit::True);
-    EXPECT_EQ(set[2], Trit::Unknown);
-    EXPECT_EQ(set[3], Trit::Unknown);
-    EXPECT_EQ(set[4], Trit::Unknown);
-    EXPECT_EQ(set[5], Trit::Unknown);
-    EXPECT_EQ(set[6], Trit::Unknown);
+    ASSERT_EQ(set[0], Trit::True);
+    ASSERT_EQ(set[1], Trit::True);
+    ASSERT_EQ(set[2], Trit::Unknown);
+    ASSERT_EQ(set[3], Trit::Unknown);
+    ASSERT_EQ(set[4], Trit::Unknown);
+    ASSERT_EQ(set[5], Trit::Unknown);
+    ASSERT_EQ(set[6], Trit::Unknown);
 }
 
 TEST(TritSetFunctionsTests, lengthTest) {
@@ -167,5 +210,5 @@ TEST(TritSetFunctionsTests, lengthTest) {
     set[1] = Trit::Unknown;
     set[2] = Trit::False;
     set[3] = Trit::Unknown;
-    EXPECT_EQ(set.length(), 3);
+    ASSERT_EQ(set.length(), 3);
 }
