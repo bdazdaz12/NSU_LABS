55e5e748d9e5228e8c0eeaf141d3e07f3ca0107f
Anatoly Evsyukov <yevsyukof@gmail.com>
2021-11-07 05:59:04 +0700

    пишем 2ю лабу по сетям

--- third-year/nets_2_labs/lab2/src/main/java/client/SendService.java
+++ third-year/nets_2_labs/lab2/src/main/java/client/SendService.java
@@ -4,31 +4,68 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class SendService {
 
-    private static final int BUFFER_SIZE = 4096;
+    private static final int BUFFER_SIZE = 8192;
+
+    private static void sendFileMetadata(BufferedOutputStream sockOutStream,
+                                         File fileToSend) throws IOException {
+        byte[] fileNameLengthBuf = ByteBuffer.allocate(2)
+                .putShort((short) fileToSend.getName().length()).array();
+
+        sockOutStream.write(fileNameLengthBuf);
+
+        byte[] fileNameBuf = fileToSend.getName().getBytes(StandardCharsets.UTF_8);
+        sockOutStream.write(fileNameBuf);
+
+        byte[] fileSizeBuf = ByteBuffer.allocate(8).putLong(fileToSend.length()).array();
+        sockOutStream.write(fileSizeBuf);
+
+        System.out.println("\n-------- Sent file metadata --------");
+    }
+
+    private static void sendFileData(BufferedOutputStream sockOutStream,
+                                     File fileToSend) throws IOException {
+        Files.copy(Path.of(fileToSend.getPath()), sockOutStream);
+        System.out.println("-------- File data sent 2--------");
+    }
+
+
+//    private static void sendFileData(BufferedOutputStream sockOutStream,
+//                                     File fileToSend) throws IOException {
+//        byte[] fileBuf = new byte[BUFFER_SIZE];
+//        long bytesSent = 0;
+//        long bytesRemain = fileToSend.length();
+//        InputStream inputStream = new FileInputStream(fileToSend);
+//        while (bytesSent < fileToSend.length()) {
+//            int bytesToSend = bytesRemain < BUFFER_SIZE ? (int) bytesRemain : 4096;
+//            int bytesReadNow = inputStream.read(fileBuf, 0, bytesToSend);
+//            sockOutStream.write(fileBuf, 0, bytesReadNow);
+//            sockOutStream.flush();
+//            bytesSent += bytesReadNow;
+//            bytesRemain -= bytesReadNow;
+//        }
+//        inputStream.close();
+//        System.out.println("-------- File data sent --------");
+//    }
 
     public static void sendFile(String filePath, String hostName, int port) {
-        String[] filePathComponents = filePath.split("/");
-        String fileToSendName = filePathComponents[filePathComponents.length - 1];
-
-        try(Socket socket = new Socket(InetAddress.getByName(hostName), port)) {
+        try (Socket socket = new Socket(InetAddress.getByName(hostName), port);
+             BufferedOutputStream sockOutStream = new BufferedOutputStream(socket.getOutputStream())) {
             File fileToSend = new File(filePath);
-            long fileSize = fileToSend.length();
-
-            BufferedInputStream fileInStream = new BufferedInputStream(Files.newInputStream(Path.of(filePath)));
-            BufferedOutputStream sockOutStream = new BufferedOutputStream(socket.getOutputStream());
-
-            byte[] buf = new byte[BUFFER_SIZE];
-
+            sendFileMetadata(sockOutStream, fileToSend);
+            sendFileData(sockOutStream, fileToSend);
         } catch (UnknownHostException e) {
-            System.err.println("INVALID SERVER ADDRESS");
+            System.err.println("UNKNOWN_HOST_ERROR: invalid server address (-_-)");
             e.printStackTrace();
             System.exit(2);
         } catch (IOException e) {
+            System.err.println("SEND_ERROR: Error during sending");
             e.printStackTrace();
             System.exit(3);
         }
---
+++ third-year/nets_2_labs/lab2/src/main/java/server/SpeedCounter.java
@@ -0,0 +1,23 @@
+package server;
+
+import java.util.TimerTask;
+
+public class SpeedCounter extends TimerTask {
+
+    long oldBytes = 0;
+    SenderThread parent;
+
+    public SpeedCounter(SenderThread parent) {
+        this.parent = parent;
+    }
+
+    @Override
+    public void run() {
+
+        long newBytes = parent.getReceivedBytes();
+        System.out.printf("Current speed of '%s': %.3f Mb/s, received %.1f %%\n", parent.getFileName(),
+                ((newBytes - oldBytes) * 1000.0 / 1000 / 8 / 1024 / 1024),
+                ((double) newBytes / parent.getFileSize() * 100.0));
+        oldBytes = newBytes;
+    }
+}
